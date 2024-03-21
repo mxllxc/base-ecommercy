eb23168aac66011fc17cd0d0fc23921207a4b306
camille <mille.ouriques@gmail.com>
2024-03-21 14:00:35 -0300

    feat: configurado pagamento de mensalidade com stripe

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,26 @@
 import './App.css'
 import Product1 from './assets/product1.jpg'
 import Product2 from './assets/product2.jpg'
-import Product3 from './assets/product3.jpg'
 
 function App() {
-
-  const produts = [
-    {
-      id: 1,
-      name: "Produto 1",
-      img: Product1,
-      price: 90
-    },
-    {
-      id: 2,
-      name: "Produto 2",
-      img: Product2,
-      price: 50
-    },
-    {
-      id: 3,
-      name: "Produto 3",
-      img: Product3,
-      price: 100
-    },
-  ]
-
-  const handlePayment = (product: any) => {
-    console.log(product);
-  }
 
   return (
     <div className='py-20 flex w-full items-center justify-center'>
       <div className='flex gap-5'>
-        {produts.map((product) => (
-          <div key={product.id} className='flex flex-col gap-3 border border-white rounded-lg'>
-            <img className='mask mask-square w-full h-full max-w-72 object-cover rounded-lg' src={product.img} alt={product.name} />
+          <div className='flex flex-col gap-3 border border-white rounded-lg'>
+            <img className='mask mask-square w-full h-full max-w-72 object-cover rounded-lg' src={Product2} alt="Plano Base" />
             <div className='px-5 pb-3 flex flex-col gap-3'>
-              <h1 className='text-white'>{product.name}</h1>
-              <button className='btn btn-primary w-full min-h-0 h-fit py-2' onClick={() => handlePayment(product)}>Buy</button>
+              <h1 className='text-white'>Plano Base</h1>
+              <button className='btn btn-primary w-full min-h-0 h-fit py-2' onClick={() => window.open("https://buy.stripe.com/test_7sI7t2ffrb2faXK7su")}>Buy</button>
             </div>
           </div>
-        ))}
+          <div className='flex flex-col gap-3 border border-white rounded-lg'>
+            <img className='mask mask-square w-full h-full max-w-72 object-cover rounded-lg' src={Product1} alt="Plano Premium" />
+            <div className='px-5 pb-3 flex flex-col gap-3'>
+              <h1 className='text-white'>Plano Premium</h1>
+              <button className='btn btn-primary w-full min-h-0 h-fit py-2' onClick={() => window.open("https://buy.stripe.com/test_bIYbJid7j4DR2recMN")}>Buy</button>
+            </div>
+          </div>
       </div>
     </div>
   )
